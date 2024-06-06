f6abb78d6a9fc0f10c924b5b31e4c719f2a3cb03
Clifford Musyoka <your_email@example.com>
2024-06-06 09:28:06 +0300

    fetch token

--- controllers/main.js
+++ controllers/main.js
@@ -7,24 +7,34 @@ const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
-    const {username, password} = req.body
+    const {username, password} = req.body;
     //check in the controller
 if(!username || !password){
-    throw new CustomAPIError('Please Provide Email and Password', 400)
+    throw new CustomAPIError('Please Provide Username and Password', 400)
 }
 
 //Just for Demo, normally provided by DB!!!
 const id = new Date().getDate()
 
 //try to keep payload small, better experience for user
-const token = jwt.sign({id, username})
+//just for demo, in production use long, complex and unguessable string Value!!!!!
+const token = jwt.sign({id, username}, process.env.JWT_SECRET,{expiresIn: '30d'})
 
+res.status(200).json({msg: 'user created', token})
 
-    
-  res.send("Fake Login/Register/Signup Route");
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if(!authHeader || !authHeader.startsWith('Bearer ')){
+    throw new CustomAPIError('No Token Provided', 401)
+  }
+
+const token = authHeader.split(' ')[1];
+
+
+
+console.log(token);
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
