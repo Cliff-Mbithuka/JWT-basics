f447c588c3ea6e2aacd6d87a46171ed3677ee0d1
Clifford Musyoka <your_email@example.com>
2024-06-07 19:23:06 +0300

    Final commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,45 +1,43 @@
-//check username, password in post(login) request
+// check username, password in post(login) request
 // if exist create new JWT
-//Send back to front-end
-//setup authentication so only the request with JWT can access the dashboard
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dasboard
 
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const { BadRequestError } = require('../errors')
 
 const login = async (req, res) => {
-    const {username, password} = req.body;
-    //check in the controller
-if(!username || !password){
-    throw new CustomAPIError('Please Provide Username and Password', 400)
-}
+  const { username, password } = req.body
+  // mongoose validation
+  // Joi
+  // check in the controller
 
-//Just for Demo, normally provided by DB!!!
-const id = new Date().getDate()
-
-//try to keep payload small, better experience for user
-//just for demo, in production use long, complex and unguessable string Value!!!!!
-const token = jwt.sign({id, username}, process.env.JWT_SECRET,{expiresIn: '30d'})
-
-res.status(200).json({msg: 'user created', token})
-
-};
-
-const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-  if(!authHeader || !authHeader.startsWith('Bearer ')){
-    throw new CustomAPIError('No Token Provided', 401)
+  if (!username || !password) {
+    throw new BadRequestError('Please provide email and password')
   }
 
-const token = authHeader.split(' ')[1];
+  //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate()
 
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
 
+  res.status(200).json({ msg: 'user created', token })
+}
 
-console.log(token);
-  const luckyNumber = Math.floor(Math.random() * 100);
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100)
+
   res.status(200).json({
-    msg: `Hello, John Doe`,
-    secret: `Here is your authorised data, your lucky number is ${luckyNumber}`,
-  });
-};
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  })
+}
 
-module.exports = { login, dashboard}
+module.exports = {
+  login,
+  dashboard,
+}
